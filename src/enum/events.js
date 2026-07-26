40a58a3c2660d06d5d1e7ed38206bdc35d3a92ac
tojo1337 <tojo.mukherjee007@gmail.com>
2026-07-26 12:29:25 +0530

    socket.io integration in progress

---
+++ src/enum/events.js
@@ -0,0 +1,7 @@
+export const EventTypes = Object.freeze({
+  sigint: "SIGINT",
+  sigterm: "SIGTERM",
+  sigquit: "SIGQUIT",
+  uncaughtException: "uncaughtException",
+  unhandledRejection: "unhandledRejection",
+});
