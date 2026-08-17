dffe5550d6b7b96703c54e5bfa3fd74d816600b7
tojo1337 <tojo.mukherjee007@gmail.com>
2026-08-17 21:50:39 +0530

    On the fly user creation flow completed

--- src/middleware/jwt.middleware.js
+++ src/middleware/jwt.middleware.js
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import mongoose from "mongoose";
 import { JwksClient } from "jwks-rsa";
 import jwt, { decode } from "jsonwebtoken";
@@ -28,72 +29,32 @@ export function getKey(header, callback) {
   });
 }
 
-/**
- * On the fly user creation handler in here
- * This is teh decoded value from a token
-{
-  iss: "https://sso.devshell.online/application/o/test-auth/",
-  sub: "522c485c5ab807c2595a7db9476e31c221789d844bc932275b311e68ee98c7c6",
-  aud: "NqFyKrwbIOqSRYkW8zMEyeekdaVlQNGFj12ujQMi",
-  exp: 1786886713,
-  iat: 1786886413,
-  auth_time: 1786886412,
-  acr: "goauthentik.io/providers/oauth2/default",
-  amr: [
-    "pwd",
-  ],
-  nonce: "6Q_aAEO6ycp4Yqp1twtlUQ",
-  sid: "7c19875a9a3f77d353eb8cbd7165f6938ba99c1a8bdbba7c521892d719cac4e2",
-  jti: "eVhmxkaMgKH3Jpyci2I8vmARQxCpgAJKi1uFaJWh",
-  email: "test@user.com",
-  email_verified: false,
-  name: "",
-  given_name: "",
-  preferred_username: "testuser",
-  nickname: "testuser",
-  groups: [
-    "react-app",
-  ],
-  azp: "NqFyKrwbIOqSRYkW8zMEyeekdaVlQNGFj12ujQMi",
-  uid: "ZLo1hXiCpTwZp5g5bEvziCDb1uWoAeUTm23VNfDQ",
-  scope: "profile email openid",
-}
- */
+// Creating user in here
 export async function createOTFUser(tokenObj) {
   try {
-    const handler = {
-      get(target, prop, receiver) {
-        if (prop === "user_id") {
-          return `${tokenObj.iss}_${tokenObj.sub}`;
-        }
-        if (prop === "name") {
-          return tokenObj.name;
-        }
-        if (prop === "email") {
-          return tokenObj.email;
-        }
-        if (prop === "is_active") {
-          return true;
-        }
-        if (prop === "is_online") {
-          return false;
-        }
-        if (prop === "socket_id") {
-          return "";
-        }
-        return Reflect.get(...arguments);
-      },
-    };
     let userUniqueId = "";
-    const proxiedUserObj = new Proxy({}, handler);
+    const userSignature = `${tokenObj.iss}_${tokenObj.sub}`;
+    const hashedUserId = crypto
+      .createHash("md5")
+      .update(userSignature)
+      .digest("hex");
+    const userObjPayload = {
+      user_id: hashedUserId,
+      name: tokenObj?.nickname,
+      email: tokenObj?.email,
+      is_active: true,
+      is_online: false,
+      socket_id: "",
+    };
     const existingUser = await UserModel.findOne({
-      user_id: proxiedUserObj.user_id,
+      user_id: hashedUserId,
     }).lean();
     if (!existingUser) {
-      const newUser = await UserModel.insertOne({ ...proxiedUserObj });
+      const newUser = await UserModel.insertOne({ ...userObjPayload });
       userUniqueId = newUser._id.toString();
+    } else if (existingUser) {
+      userUniqueId = existingUser._id.toString();
     }
-    userUniqueId = existingUser._id.toString();
     return userUniqueId;
   } catch (err) {
     throw err;
@@ -117,13 +78,12 @@ export default async function jwtMiddleware(req, res, next) {
   try {
     const token = req.headers.authorization?.split("Bearer ")[1] || "";
     const decodedTokenObj = await deocdeToken(token);
-    // Check if works then proceed
-    // const userId = await createOTFUser(decodedTokenObj);
-    // const payload = {
-    //   id: decoded.id || ""
-    // };
-    // req.userData = payload;
-    // return next();
+    const userId = await createOTFUser(decodedTokenObj);
+    const payload = {
+      id: userId || "",
+    };
+    req.userData = payload;
+    return next();
   } catch (err) {
     return res
       .status(HttpStatus.UN_AUTHORIZED)
