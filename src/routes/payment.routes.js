76434ec7a5705fe38afcf4fbd2eab1390e300fa0
tojo1337 <tojo.mukherjee007@gmail.com>
2026-09-24 22:19:29 +0530

    plan routes WIP

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -8,14 +8,68 @@ import { agenda } from "../config/agenda.config.js";
 import { AgendaJobs } from "../enum/agenda-jobs.js";
 import { PlansMapperModel } from "../model/plan-mapper.model.js";
 import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils.js";
+import { jwtMiddleware } from "../middleware/jwt.middleware.js";
+import * as helper from "../utils/helper.js";
+import mongoose from "mongoose";
 
 const route = Router();
 const paymentGateway = gateway;
 
-route.get("/get-all-produces", async (req, res) => {
+route.use(jwtMiddleware);
+
+route.get("/get-all-plans", async (req, res) => {
   try {
     const productData = await PlansModel.find({}).lean();
     return res.status(HttpStatus.OK).json({ data: productData || [] });
+  } catch (err) {
+    logger.error({
+      url: req.originalUrl,
+      method: req.method,
+      body: req.body,
+      stack: err.stack,
+    });
+    return res
+      .status(HttpStatus.SERVER_ERROR)
+      .json({ message: "Some error occurred" });
+  }
+});
+
+route.get("/get-active-plan", async (req, res) => {
+  try {
+    const { id = "" } = req.userData || {};
+    const [mapped_plan, all_plan_data] = await helper.promiseCaller([
+      () =>
+        PlansMapperModel.findOne({
+          user_id: new mongoose.Types.ObjectId(id),
+          created_at: -1,
+        }).lean(),
+      () => PlansModel.find({}).lean(),
+    ]);
+    let response_plan = all_plan_data.reduce((acc, item) => {
+      if (item.plan_id === mapped_plan.plan_id) {
+        acc = item;
+      }
+      return acc;
+    }, null);
+    if (!response_plan) {
+      const basic_plan = await PlansModel.find({
+        plan_name: "Basic",
+      }).lean();
+      await PlansMapperModel.findOneAndUpdate(
+        { user_id: new mongoose.Types.ObjectId(id), created_at: -1 },
+        {
+          subscription_id: helper.genUuid(),
+          user_id: new mongoose.Types.ObjectId(id),
+          plan_id: basic_plan._id,
+        },
+        {
+          new: true,
+          upsert: true,
+        },
+      );
+      return res.status(HttpStatus.ERROR).json({ data: basic_plan });
+    }
+    return res.status(HttpStatus.OK).send({ data: response_plan });
   } catch (err) {
     logger.error({
       url: req.originalUrl,
